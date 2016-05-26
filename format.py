e065f08d71609de44151ecdd9d9cb152dbf8713b
Daniel Izquierdo <dizquierdo@bitergia.com>
2016-05-26 19:34:07 +0200

    Add module to deal with format and unification of data
    Methods receive as entry a pandas.DataFrame.
    Those return a new dataframe with some basic formating activity such as
    adding, removing, filling and dates formating.

---
+++ format.py
@@ -0,0 +1,113 @@
+#!/usr/bin/python
+# Copyright (C) 2016 Bitergia
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+# Authors:
+#   Daniel Izquierdo Cortazar <dizquierdo@bitergia.com>
+#
+
+
+import pandas
+
+import scipy
+
+import datetime
+
+class Format(object):
+    """ Abstract class for the several data sources
+
+    This class aims at being the central point where all of the JSON
+    files coming from Perceval are unified. This data format and
+    cleaning process is done due to inconsistencies and missing
+    fields that may appear when reading Perceval info.
+
+    Thus, the goal of this class and its subclasses is to provide
+    a trustable final JSON format for each of the data sources.
+    """
+
+    def fill_missing_fields(self, data, columns):
+        """ This  method fills with 0's missing fields
+
+        :param data: original Pandas dataframe
+        :param columns: list of columns to be filled in the DataFrame
+        :type data: pandas.DataFrame
+        :type columns: list of strings
+
+        :returns: Pandas dataframe with missing fields filled with 0's
+        :rtype: pandas.DataFrame
+        """
+
+        for column in columns:
+            if column not in data.columns:
+                data[column] = scipy.zeros(len(data))
+
+        return data
+
+    def update_field_names(self, data, matching):
+        """ This method updates the names of the fields according to matching
+
+        :param data: original Pandas dataframe
+        :param matching: dictionary of matchings between old and new values
+        :type data: pandas.DataFrame
+        :type matching: dictionary
+
+        :returns: Pandas dataframe with updated names
+        :rtype: pandas.DataFrame
+        """
+
+        for key in matching.keys():
+            if key in data.columns:
+                data.rename(columns={key:matching[key]})
+
+        return data
+
+
+    def format_dates(self, data, columns):
+        """ This method translates columns values into datetime objects
+
+        :param data: original Pandas dataframe
+        :param columns: list of columns to cast the date to a datetime object
+        :type data: pandas.DataFrame
+        :type columns: list of strings
+
+        :returns: Pandas dataframe with updated 'columns' with datetime objects
+        :rtype: pandas.DataFrame
+        """
+
+        for column in columns:
+            if column in data.columns:
+                data[column] = pandas.to_datetime(data[column])
+
+        return data
+
+    def remove_columns(self, data, columns):
+        """ This method removes columns in data
+
+        :param data: original Pandas dataframe
+        :param columns: list of columns to remove
+        :type data: pandas.DataFrame
+        :type columns: list of strings
+
+        :returns: Pandas dataframe with removed columns
+        :rtype: pandas.DataFrame
+        """
+
+        for column in columns:
+            if column in data.columns:
+                data = data.drop(column, axis=1)
+
+        return data
+
---
+++ tests/test_format.py
@@ -0,0 +1,55 @@
+# Copyright (C) 2016 Bitergia
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+# Authors:
+#     Daniel Izquierdo <dizquierdo@bitergia.com>
+#
+
+import sys
+
+import unittest
+
+import pandas
+
+import scipy
+
+if not '..' in sys.path:
+    sys.path.insert(0, '..')
+
+from format import Format
+
+class TestFormat(unittest.TestCase):
+    """ Unit tests for Format class
+    """
+
+    def test_fill_missing_fields(self):
+        """ Test several cases for the fill_missing_fields method
+        """
+
+        empty_columns = []
+        columns = ["test1"]
+        empty_df = pandas.DataFrame()
+        # With empty dataframe and any columns, this always returns empty dataframe
+        # A DataFrame with columns but not data is an empty DataFrame
+        self.assertTrue(Format().fill_missing_fields(empty_df, empty_columns).empty)
+        self.assertTrue(Format().fill_missing_fields(empty_df, columns).empty)
+        self.assertEqual(columns, Format().fill_missing_fields(empty_df, columns).columns)
+
+        # With a dataframe with some data, this returns a non-empty dataframe
+        df = empty_df.copy()
+        df["test"] = scipy.zeros(10)
+
+        self.assertFalse(Format().fill_missing_fields(df, empty_columns).empty)
