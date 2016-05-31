b793a417459f50181a8e856762005f768506dc06
Daniel Izquierdo <dizquierdo@bitergia.com>
2016-05-31 13:24:54 +0200

    Add a first level of events for Gerrit activity

--- events.py
+++ events.py
@@ -21,6 +21,8 @@
 
 from dateutil import parser
 
+from datetime import datetime
+
 import pandas
 
 class Events(object):
@@ -192,6 +194,7 @@ class Git(Events):
                 commit[Git.COMMIT_DATE].append(parser.parse(commit_data['AuthorDate']))
                 commit[Git.COMMIT_OWNER].append(commit_data['Author'])
 
+            #TODO: this will fail if no files are found in a commit (eg: merge)
             if granularity == 2:
                 # Add extra info about files actions, if there were any
                 if commit_data.has_key("files"):
@@ -228,3 +231,91 @@ class Git(Events):
             events[Git.FILE_REMOVED_LINES] = commit[Git.FILE_REMOVED_LINES]
 
         return events
+
+
+
+class Gerrit(Events):
+    """ Class used to 'eventize' Gerrit items
+
+    This splits information of each item based on a pre-configured mapping.
+    There are several levels of events. These levels were created as a way
+    to have more or less time consuming generation of events.
+    """
+
+    EVENT_OPEN = "CHANGESET_SENT"
+    EVENT_ = "CHANGESET_"
+
+    # Fields supported by this module (when a DataFrame is returned)
+    CHANGESET_ID = "id"
+    CHANGESET_EVENT = "eventtype"
+    CHANGESET_DATE = "date"
+    CHANGESET_OWNER = "owner"
+
+    def __init__(self, items):
+        """ Main constructor of the class
+
+        :param items: original list of JSON that contains all info about a commit
+        :type items: list
+        """
+
+        self.items = items
+
+    def eventize(self, granularity):
+        """ This splits the JSON information found at self.events into the
+        several events. For this there are three different levels of time
+        consuming actions: 1-soft, 2-medium and 3-hard.
+
+        Level 1 provides events about commits
+        Level 2 provides events about files
+        Level 3 provides other events (not used so far)
+
+        :param granularity: Levels of time consuming actions to calculate events
+        :type granularity: integer
+
+        :returns: Pandas dataframe with splitted events.
+        :rtype: pandas.DataFrame
+        """
+
+        changeset = {}
+        # First level granularity
+        changeset[Gerrit.CHANGESET_ID] = []
+        changeset[Gerrit.CHANGESET_EVENT] = []
+        changeset[Gerrit.CHANGESET_DATE] = []
+        changeset[Gerrit.CHANGESET_OWNER] = []
+
+        events = pandas.DataFrame()
+
+        for item in self.items:
+            changeset_data = item["data"]
+            if granularity == 1:
+                # Changeset submission date: filling a new event
+                changeset[Gerrit.CHANGESET_ID].append(changeset_data["number"])
+                changeset[Gerrit.CHANGESET_EVENT].append(Gerrit.EVENT_OPEN)
+                changeset[Gerrit.CHANGESET_DATE].append(datetime.fromtimestamp(int(changeset_data["createdOn"])))
+                changeset[Gerrit.CHANGESET_OWNER].append(changeset_data["owner"]["username"])
+
+                # Adding the closing status updates (if there was any)
+                if changeset_data["status"] == 'ABANDONED' or \
+                   changeset_data["status"] == 'MERGED':
+                       closing_date = datetime.fromtimestamp(int(changeset_data["lastUpdated"]))
+                       changeset[Gerrit.CHANGESET_ID].append(changeset_data["number"])
+                       changeset[Gerrit.CHANGESET_EVENT].append(Gerrit.EVENT_ + changeset_data["status"])
+                       changeset[Gerrit.CHANGESET_DATE].append(closing_date)
+                       changeset[Gerrit.CHANGESET_OWNER].append(changeset_data["owner"]["username"])
+
+            if granularity == 2:
+                #TDB
+                pass
+
+            if granularity == 3:
+                #TDB
+                pass
+
+        # Done in this way to have an order (and not a direct cast)
+        events[Gerrit.CHANGESET_ID] = changeset[Gerrit.CHANGESET_ID]
+        events[Gerrit.CHANGESET_EVENT] = changeset[Gerrit.CHANGESET_EVENT]
+        events[Gerrit.CHANGESET_DATE] = changeset[Gerrit.CHANGESET_DATE]
+        events[Gerrit.CHANGESET_OWNER] = changeset[Gerrit.CHANGESET_OWNER]
+
+        return events
+
