86ba8cdf0e978f7cc66ea36d852886b643556602
Daniel Izquierdo <dizquierdo@bitergia.com>
2017-04-28 10:59:59 +0200

    Update OpenStack Diversity analysis Ceres client

--- examples/openstack_gender.py
+++ examples/openstack_gender.py
@@ -34,7 +34,7 @@ import events
 
 import certifi
 
-from enrich import Gender, FileType, EmailFlag, SplitLists, MaxMin, SplitEmail, ToUTF8
+from enrich import Gender, FileType, EmailFlag, SplitLists, MaxMin, SplitEmail, ToUTF8, Uuid
 
 from filter import FilterRows
 
@@ -47,23 +47,25 @@ MAPPING_GERRIT = {
                 "type": "date"
             },
             "owner": {
-                "index": "not_analyzed",
-                "type": "string"
+                "type": "keyword"
             },
             "id": {
-                "type": "string"
+                "type": "keyword"
             },
             "eventtype": {
-                "index": "not_analyzed",
-                "type": "string"
+                "type": "keyword"
             },
             "repository": {
-                "index": "not_analyzed",
-                "type": "string"
+                "type": "keyword"
             },
             "projects": {
-                "index": "not_analyzed",
-                "type": "string"
+                "type": "keyword"
+            },
+            "gender": {
+                "type": "keyword"
+            },
+            "uuid": {
+                "type": "keyword"
             }
             }
         }
@@ -106,6 +108,12 @@ MAPPING_GIT = {
             "projects": {
                 "index": "not_analyzed",
                 "type": "string"
+            },
+            "gender": {
+                "type": "keyword"
+            },
+            "uuid": {
+                "type": "keyword"
             }
         }
     }
@@ -144,10 +152,10 @@ def ESConnection():
 
     connection = "https://" + user + ":" + password + "@" + host + ":" + port + "/"
     print (connection)
-    es_read = Elasticsearch([connection], use_ssl=True, verity_certs=True, ca_cert=certifi.where(), scroll='300m')
+    es_read = Elasticsearch([connection], use_ssl=True, verity_certs=True, ca_cert=certifi.where(), scroll='300m', timeout=100)
 
     connection_output = "https://" + user_output + ":" + password_output + "@" + host_output + ":" + port_output + "/" + path_output
-    es_write = Elasticsearch([connection_output], use_ssl=True, verity_certs=True, ca_cert=certifi.where(), scroll='300m')
+    es_write = Elasticsearch([connection_output], use_ssl=True, verity_certs=True, ca_cert=certifi.where(), scroll='300m', timeout=100)
 
     return es_read, es_write, es_read_git_index, es_read_gerrit_index, \
            es_write_git_index, es_write_gerrit_index, genderize_key
@@ -183,7 +191,14 @@ def openstack_projects():
 
 def analyze_gerrit(es_read, es_write, es_read_index, es_write_index, key):
 
+    # Retrieve projects info
     projects = openstack_projects()
+
+    # Retrieve uuids info
+    uuids = Uuid(pandas.DataFrame(), file_path='openstack_uuids.csv')
+
+    # Retrieve gender cached data
+    enriched_gender = Gender(pandas.DataFrame(), key, "gerrit_gender.csv")
 
     es_write.indices.delete(es_write_index, ignore=[400, 404])
     es_write.indices.create(es_write_index, body=MAPPING_GERRIT)
@@ -198,50 +213,73 @@ def analyze_gerrit(es_read, es_write, es_read_index, es_write_index, key):
     for item in helpers.scan(es_read, query, scroll='300m', index=es_read_index):
         items.append(item["_source"])
 
-        if cont % 10000 == 0:
+        if cont % 15000 == 0:
             # Eventizing the first 7500 changesets
             gerrit_events = events.Gerrit(items)
             events_df = gerrit_events.eventize(2)
 
             print (cont)
-
+            print (len(events_df))
             # Adding projects information
-            print (events_df.columns)
-            print (projects.columns)
             events_df = pandas.merge(events_df, projects, how='left', on='repository')
 
             # Adding gender info
-            if first:
-                enriched_gender = Gender(events_df, key)
-                first = False
-            else:
-                enriched_gender.data = events_df
+            enriched_gender.data = events_df
 
             events_df = enriched_gender.enrich("owner")
             events_df = events_df.fillna("Unknown")
 
+            print (len(events_df))
+            print (events_df.keys())
+            # Add author uuid
+            uuids.data = events_df
+            events_df["user"] = events_df["owner"]
+            events_df = uuids.enrich(['user','email'])
+
+            print (len(events_df))
+            print (events_df.keys())
             # Uploading info to the new ES
-            test = events_df.to_dict("index")
-            docs = []
-            for i in test.keys():
-                header = {
-                      "_index": es_write_index,
-                      "_type": "item",
-                      "_id": int(uniq_id),
-                      "_source": test[i]
-                    }
-                docs.append(header)
-                uniq_id = uniq_id + 1
-            print (len(docs))
-            helpers.bulk(es_write, docs)
+            uniq_id = upload_data(events_df, es_write_index, es_write, uniq_id)
+
             items = []
+
         cont = cont + 1
+
+    #helpers.bulk(es_write, docs)
+    uniq_id = upload_data(events_df, es_write_index, es_write, uniq_id)
+
+def upload_data(events_df, es_write_index, es_write, uniq_id):
+    # Uploading info to the new ES
+    test = events_df.to_dict("index")
+    docs = []
+    for i in test.keys():
+        header = {
+               "_index": es_write_index,
+               "_type": "item",
+               "_id": int(uniq_id),
+               "_source": test[i]
+        }
+        docs.append(header)
+        uniq_id = uniq_id + 1
+    print (len(docs))
     helpers.bulk(es_write, docs)
+    items = []
+
+    return uniq_id
 
 
 def analyze_git(es_read, es_write, es_read_index, es_write_index, key):
 
+    # Retrieve projects information
     projects = openstack_projects()
+    projects["repository"] = projects["urls"]
+    projects = projects.drop("urls", 1)
+
+    # Retrieve uuids info
+    uuids = Uuid(pandas.DataFrame(), file_path='openstack_uuids.csv')
+
+    # Retrieve gender info
+    enriched_gender = Gender(pandas.DataFrame(), key, "git_gender.csv")
 
     es_write.indices.delete(es_write_index, ignore=[400, 404])
     es_write.indices.create(es_write_index, body=MAPPING_GIT)
@@ -264,6 +302,7 @@ def analyze_git(es_read, es_write, es_read_index, es_write_index, key):
             events_df = git_events.eventize(2)
 
             print (cont)
+            print (len(events_df))
             # Filter information
             data_filtered = FilterRows(events_df)
             events_df = data_filtered.filter_(["filepath"], "-")
@@ -272,12 +311,7 @@ def analyze_git(es_read, es_write, es_read_index, es_write_index, key):
             enriched_filetype = FileType(events_df)
             events_df = enriched_filetype.enrich('filepath')
 
-            # Add gender info
-            if first:
-                enriched_gender = Gender(events_df, key)
-                first = False
-            else:
-                enriched_gender.data = events_df
+            enriched_gender.data = events_df
 
             events_df = enriched_gender.enrich("owner")
             aux = pandas.DataFrame(events_df["committer"], columns=["committer"])
@@ -292,37 +326,27 @@ def analyze_git(es_read, es_write, es_read_index, es_write_index, key):
             splitemail = SplitEmail(events_df)
             events_df = splitemail.enrich("owner")
 
-            # Add projects information
-            projects["repository"] = projects["urls"]
-            projects = projects.drop("urls", 1)
-            
-            events_df = pandas.merge(events_df, projects, how='left', on='repository')
+            # Add author uuid
+            uuids.data = events_df
+            events_df = uuids.enrich(['user', 'email'])
 
-            #all_files = all_files.append(events_df)
-            #test = events_df.to_dict("index")
-            
+            print (len(events_df))
+            # Add projects information
+            events_df = pandas.merge(events_df, projects, how='left', on='repository')
+            # Fill NaN projects
+            events_df.fillna('notavailable', inplace=True)
+
             # Deal with surrogates
             convert = ToUTF8(events_df)
-            print (events_df.columns)
-            events_df = convert.enrich(["gender_analyzed_name", "committer_gender_analyzed_name", "owner", "committer", "user"])
-            commits = []
-#    max_min = MaxMin(all_files)
-#    all_files = max_min.enrich(["date"], "owner")
+            events_df = convert.enrich(["gender_analyzed_name", "committer_gender_analyzed_name", "owner", "committer", "user", "username"])
 
-            test = events_df.to_dict("index")
-            docs = []
-            for i in test.keys():
-                header = {
-                      "_index": es_write_index,
-                      "_type": "item",
-                      "_id": int(uniq_id),
-                      "_source": test[i]
-                    }
-                docs.append(header)
-                uniq_id = uniq_id + 1
-            helpers.bulk(es_write, docs)
+            print (len(events_df))
+            commits = []
+
+            uniq_id = upload_data(events_df, es_write_index, es_write, uniq_id)
+
         cont = cont + 1
-    helpers.bulk(es_write, docs)
+    upload_data(events_df, es_write_index, es_write, uniq_id)
 
 def main():
 
