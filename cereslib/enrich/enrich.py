fc3b0b9aced9f6be3aebb8c6c8a94832d79013b3
Luis Cañas-Díaz <lcanas@bitergia.com>
2023-01-13 12:48:22 +0100

    Add more file extensions to FileType method
    Changes modify the regular expression with file extensions to
    include the following languages:
      - TypeScript (.ts)
      - JavaScrit (.mjs)
      - Bazel (.bazel, .bzl and .bazelrc)
      - Shell (.sh)
      - Terraform (.tf)
    
    To make easier the maintenance of the list, the new one is ordered
    by name.
    
    Signed-off-by: Luis Cañas-Díaz <lcanas@bitergia.com>

--- cereslib/enrich/enrich.py
+++ cereslib/enrich/enrich.py
@@ -129,7 +129,8 @@ class FileType(Enrich):
 
         # Insert 'Code' only in those rows that are
         # detected as being source code thanks to its extension
-        reg = "\.c$|\.h$|\.cc$|\.cpp$|\.cxx$|\.c\+\+$|\.cp$|\.py$|\.js$|\.java$|\.rs$|\.go$"
+        reg = "\.bazel$|\.bazelrc$|\.bzl$|\.c$|\.cc$|\.cp$|\.cpp$|\.cxx$|\.c\+\+$|" +\
+              "\.go$|\.h$|\.js$|\.mjs$|\.java$|\.py$|\.rs$|\.sh$|\.tf$|\.ts$"
         self.data.loc[self.data[column].str.contains(reg), 'filetype'] = 'Code'
 
         return self.data
